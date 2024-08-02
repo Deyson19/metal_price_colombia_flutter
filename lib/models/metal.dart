531ac2001a3dbb737ce83891044fd5f90e875a81
Deyson Venté <deiandresvent@hotmail.com>
2024-08-02 00:54:16 -0500

    Modelo para crear los diferentes tipos de metales.

---
+++ lib/models/metal.dart
@@ -0,0 +1,12 @@
+class Metal{
+  final String nombre;
+  final double precioCompra;
+  final double precioVenta;
+  
+  Metal({
+    required this.nombre,
+    required this.precioCompra,
+    required this.precioVenta
+  });  
+
+}
